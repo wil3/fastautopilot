3fc0a503f305ee18b5438b185ce62b557cf43aec
Wil <wfkoch@gmail.com>
2017-09-20 21:54:22 -0400

    more tests

--- fastautopilot/tests/test_gazeboapi.py
+++ fastautopilot/tests/test_gazeboapi.py
@@ -1,19 +1,58 @@
-
+import os
+import logging.config
+import yaml
 import unittest
 import sys
 sys.path.append("..")
-from tracker import FlightData
+import time
+from sim.gazebo.gazeboapi import GazeboAPI 
 
 class TestGazeboAPI(unittest.TestCase):
+    def test_reset(self):
+        gazebo_host="127.0.0.1"
+        gazebo_port=11345
+        GazeboAPI(gazebo_host, gazebo_port).test()
+    """
 
     def test_reset(self):
-        pass
+        gazebo_host="127.0.0.1"
+        gazebo_port=11345
+        def reset_callback():
+            print "Reset"
+        GazeboAPI(gazebo_host, gazebo_port).reset_model(reset_callback)
+
+    def test_reset_multiple(self):
+        gazebo_host="127.0.0.1"
+        gazebo_port=11345
+        
+        self.counter = -1 
+        def reset_callback():
+            print "Reset ", self.counter
+            self.counter += 1
+
+        for i in range(10):
+            GazeboAPI(gazebo_host, gazebo_port).reset_model(reset_callback)
+            while i != self.counter:
+                time.sleep(1)
+
 
     def test_sim_time(self):
         world_stat = GazeboAPI("localhost", 11345)
         def sim_time_callback(sim_time):
             print "Start Sim Time", sim_time.sec
         world_stat.get_sim_time(sim_time_callback)
+    """
+def init_logging():
+    log_config = os.path.join(".", "../../conf/logging.yaml")
+    if os.path.exists(log_config):
+        with open(log_config, 'rt') as f:
+            log_config = yaml.load(f.read())
+        logging.config.dictConfig(log_config)
+    else:
+        raise Exception("Cannot find log configuration file")
+
+
 
 if __name__ == "__main__":
+    init_logging()
     unittest.main()
